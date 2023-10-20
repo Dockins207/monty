5ab6e44056744325c315d2b7cbdf779aa441f730
Dockins <129792373+Dockins207@users.noreply.github.com>
2023-10-20 10:40:38 +0300

    C - Stacks, Queues - LIFO, FIFO

--- monty_run.c
+++ monty_run.c
@@ -1,9 +1,3 @@
-/*
- * File: run_monty.c
- * Auth: Donna Govender
- *       Muyiwa Fatunsin
- */
-
 #include "monty.h"
 #include <string.h>
 
@@ -31,7 +25,6 @@ void free_tokens(void)
 
 /**
  * token_arr_len - Gets the length of current op_toks.
- *
  * Return: Length of current op_toks (as int).
  */
 unsigned int token_arr_len(void)
@@ -47,7 +40,6 @@ unsigned int token_arr_len(void)
  * is_empty_line - Checks if a line read from getline only contains delimiters.
  * @line: A pointer to the line.
  * @delims: A string of delimiter characters.
- *
  * Return: If the line only contains delimiters - 1.
  *         Otherwise - 0.
  */
@@ -72,7 +64,6 @@ int is_empty_line(char *line, char *delims)
 /**
  * get_op_func - Matches an opcode with its corresponding function.
  * @opcode: The opcode to match.
- *
  * Return: A pointer to the corresponding function.
  */
 void (*get_op_func(char *opcode))(stack_t**, unsigned int)
@@ -111,7 +102,6 @@ void (*get_op_func(char *opcode))(stack_t**, unsigned int)
 /**
  * run_monty - Primary function to execute a Monty bytecodes script.
  * @script_fd: File descriptor for an open Monty bytecodes script.
- *
  * Return: EXIT_SUCCESS on success, respective error code on failure.
  */
 int run_monty(FILE *script_fd)
