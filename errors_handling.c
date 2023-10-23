15d2b0cc8e962b5189fa1facb7568223b6d68fbf
Dockins <129792373+Dockins207@users.noreply.github.com>
2023-10-24 00:14:27 +0300

    C - Stacks, Queues - LIFO, FIFO

--- errors_handling.c
+++ errors_handling.c
@@ -1,17 +1,6 @@
 #include "monty.h"
 
-/**
- * err - Prints appropiate error messages determined by their error code.
- * @error_code: The error codes are the following:
- * (1) => The user does not give any file or more than one file to the program.
- * (2) => The file provided is not a file that can be opened or read.
- * (3) => The file provided contains an invalid instruction.
- * (4) => When the program is unable to malloc more memory.
- * (5) => When the parameter passed to the instruction "push" is not an int.
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation.
- */
+
 void err(int error_code, ...)
 {
 	va_list ag;
@@ -46,14 +35,6 @@ void err(int error_code, ...)
 	exit(EXIT_FAILURE);
 }
 
-/**
- * more_err - handles errors.
- * @error_code: The error codes are the following:
- * (6) => When the stack it empty for pint.
- * (7) => When the stack it empty for pop.
- * (8) => When stack is too short for operation.
- * (9) => Division by zero.
- */
 void more_err(int error_code, ...)
 {
 	va_list ag;
@@ -87,12 +68,6 @@ void more_err(int error_code, ...)
 	exit(EXIT_FAILURE);
 }
 
-/**
- * string_err - handles errors.
- * @error_code: The error codes are the following:
- * (10) ~> The number inside a node is outside ASCII bounds.
- * (11) ~> The stack is empty.
- */
 void string_err(int error_code, ...)
 {
 	va_list ag;
