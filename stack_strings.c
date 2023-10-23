46a4d8a6579addbeb31db10261cc67442d1e0741
Dockins <129792373+Dockins207@users.noreply.github.com>
2023-10-24 00:19:26 +0300

    C - Stacks, Queues - LIFO, FIFO

--- stack_strings.c
+++ stack_strings.c
@@ -1,10 +1,5 @@
 #include "monty.h"
 
-/**
- * print_char - Prints the Ascii value.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
- */
 void print_char(stack_t **stack, unsigned int line_number)
 {
 	int ascii;
